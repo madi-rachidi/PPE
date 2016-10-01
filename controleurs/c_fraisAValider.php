ebce0ca0d0375ff0cc228c2b02e86b7244cd1ea1
madi <madi@10.20.129.71>
2016-10-01 21:04:49 +0200

    dernier 01/10/2016 Armel

--- controleurs/c_fraisAValider.php
+++ controleurs/c_fraisAValider.php
@@ -8,9 +8,17 @@ switch($action){
 		// Afin de sélectionner par défaut le dernier mois dans la zone de liste
 		// on demande toutes les clés, et on prend la première,
 		// les mois étant triés décroissants
+                array_filter($lesMois); //va clean le tableau et supprimer les lignes qui aurons des valeurs null, vides ou false.
+                if(empty($lesMois)){
+                   ajouterErreur("Pas de fiche frais a valider ce mois-ci !");
+			include("vues/v_erreurs.php");
+                }
+                else
+                    {
 		$lesCles = array_keys( $lesMois );
 		$moisASelectionner = $lesCles[0];
 		include("vues/v_listMoisAValider.php");
+                }
 		break;
         }
         case 'visiteurFraisAValider':{
@@ -20,31 +28,33 @@ switch($action){
 		$moisASelectionner = $leMois;
                /* var_dump($moisASelectionner);*/ //debug
 		include("vues/v_listMoisAValider.php");
+                $numAnnee =substr( $leMois,0,4); /*modifie le formatage de l'année*/
+		$numMois =substr( $leMois,4,2); /*modifie le formatage du mois*/
 		$aValider=$pdo->getLesFicheFraisAValider($leMois);
-                $title = [IdVisiteur, Nom, Prenom]; // pour l'affichage
-                /*var_dump($lesMois);*///debug
-                if (! is_array($aValider)){
-                   echo"Aucune fiche Frais a valider"; 
-                }
                 include("vues/v_afficheVisiteur.php");
-                /*else{
-                    foreach ($aValider as list($a, $b, $c)) {
-                      // $a contient le premier élément du tableau interne,
-                     // et $b contient le second élément.
-                        echo "A: $a; B: $b; C: $c\n  ";
-                }
-                
-}*/
-		
+                break;
             }
         case "voiFraisAValider":{
-            $lesMois=$pdo->getLesMoisAValider();
-                $leMois = $_REQUEST['lstMois'];
-		$lesCles = array_keys( $lesMois );
+                 $valeur = $_REQUEST["valeur"];
+                 $leMois = $_REQUEST["date"];
+                 var_dump($valeur);
+                 var_dump($leMois);
+                 /**/
+                 $leMois = $_REQUEST['lstMois']; 
+		$lesMois=$pdo->getLesMoisDisponibles($idVisiteur);
 		$moisASelectionner = $leMois;
-            echo"en fab oklm et ";
-            
-            echo"$_GET[$valeur]";
+                 /**/
+                $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($valeur,$leMois);
+		$lesFraisForfait= $pdo->getLesFraisForfait($valeur,$leMois);
+		$lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($valeur,$leMois);
+		$numAnnee =substr( $leMois,0,4);
+		$numMois =substr( $leMois,4,2);
+		$libEtat = $lesInfosFicheFrais['libEtat'];
+		$montantValide = $lesInfosFicheFrais['montantValide'];
+		$nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
+		$dateModif =  $lesInfosFicheFrais['dateModif'];
+		$dateModif =  dateAnglaisVersFrancais($dateModif);
+		include("vues/v_etatFrais.php");
             
             
         }
--- vues/v_afficheVisiteur.php
+++ vues/v_afficheVisiteur.php
@@ -1,12 +1,13 @@
 ﻿
 <h3>Fiche de frais a valider du mois <?php echo $numMois."-".$numAnnee?> : 
     </h3>
+    <?php $title = [Idvisiteur,Nom, Prenom]; // pour l'affichage ?>
     <div class="encadre">
   	<table class="listeLegere">
   	   <caption>FICHE</caption>
         <tr>
          <?php
-         for ($i=0;$i<3; $i++)
+         for ($i=0;$i<2; $i++)
 		 {
 			/*$libelle = $unFraisForfait['libelle'];*/
 		?>	
@@ -19,9 +20,9 @@
         <?php
           foreach ($aValider as list($a, $b, $c)) {
             ?>
-            <td class="qteForfait"> <a href="<?php echo"index.php?uc=fraisAValider&action=voiFraisAValider&valeur=".$a ?>" > <?php echo $a?></a></td>
-            <td class="qteForfait"><?php echo $b?></td>
-            <td class="qteForfait"><?php echo $c?></td>
+            <td class="qteForfait"><a href="index.php?uc=fraisAValider&action=voiFraisAValider&valeur=<?php echo $a?>&date=<?php echo $leMois?>"><?php echo $a?></a></td>
+            <td class="qteForfait"><a href="index.php?uc=fraisAValider&action=voiFraisAValider"><?php echo $b?></a></td>
+            <td class="qteForfait"><a href="index.php?uc=fraisAValider&action=voiFraisAValider"><?php echo $c?></a></td>
          </tr>
 		 <?php
           }
@@ -29,7 +30,6 @@
 	
     </table>
   	
-  </div>
   </div>
  
 
